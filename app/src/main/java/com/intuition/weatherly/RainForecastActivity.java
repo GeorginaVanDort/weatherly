7fdfcca63cfb2c4355699bedea4b6690371108fc
g <georginavandort@gmail.com>
2017-05-25 16:26:29 -0700

    add initial files and gradient drawable

---
+++ app/src/main/java/com/intuition/weatherly/RainForecastActivity.java
@@ -0,0 +1,13 @@
+package com.intuition.weatherly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RainForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rain_forecast);
+    }
+}
---
+++ app/src/main/java/com/intuition/weatherly/WeatherDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.intuition.weatherly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_display);
+    }
+}
