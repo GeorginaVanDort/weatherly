01ab90e7fc219008fc76c5c3e1d6d99319c70f6a
g <georginavandort@gmail.com>
2017-05-26 09:47:03 -0700

    bind views to layout on main activity

--- app/src/main/java/com/intuition/weatherly/MainActivity.java
+++ app/src/main/java/com/intuition/weatherly/MainActivity.java
@@ -1,13 +1,33 @@
 package com.intuition.weatherly;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.appTitle) TextView mAppTitle;
+    @BindView(R.id.enterCityText) TextView mEnterCityText;
+    @BindView(R.id.cityInput) EditText mCityInput;
+    @BindView(R.id.addCityButton) Button mAddCityButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        Typeface pacifico = Typeface.createFromAsset(getAssets(),"fonts/pacifico.ttf");
+        mAppTitle.setTypeface(pacifico);
+
+
+
     }
 }
