14cfec5d8279fd848bdf3ca33c026732bd450c8f
g <georginavandort@gmail.com>
2017-06-07 14:07:49 -0700

    add some new login edit text views

---
+++ app/src/main/java/com/intuition/weatherly/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.intuition.weatherly.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.intuition.weatherly.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
