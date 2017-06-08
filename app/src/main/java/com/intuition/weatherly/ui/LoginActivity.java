cb7de749bd9de7558d09e82280bff568b203b86c
g <georginavandort@gmail.com>
2017-06-08 14:15:05 -0700

    connect login screen to firebase with Auth

--- app/src/main/java/com/intuition/weatherly/ui/LoginActivity.java
+++ app/src/main/java/com/intuition/weatherly/ui/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.intuition.weatherly.ui;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +18,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.intuition.weatherly.R;
 
 import butterknife.BindView;
@@ -24,6 +28,11 @@ import butterknife.ButterKnife;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private ProgressDialog mProgressDialog;
+    private ProgressDialog mLoginDialog;
+    public static final String TAG = LoginActivity.class.getSimpleName();
+    private String mUserName;
 
     @BindView(R.id.loginButt) TextView mLoginButt;
     @BindView(R.id.loginEmail) TextView mLoginEmail;
@@ -54,11 +63,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mLoginButt.setOnClickListener(this);
         mCreateAccButt.setOnClickListener(this);
         mCreateAccOk.setOnClickListener(this);
+        mLoginOk.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
-
-
+        createAuthStateListener();
+        createAuthProgressDialog();
+        createLoginDialog();
     }
+
+
 
     @Override
     public void onClick(View v) {
@@ -75,8 +88,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
 
         if (v == mCreateAccOk) {
-            Log.v("HEY", "EJERJERJEWRWEEWRKEWRKEWRKWER");
             createNewUser();
+        }
+
+        if (v == mLoginOk) {
+            timeToLogin();
+        }
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
         }
     }
 
@@ -98,29 +130,35 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void createNewUser() {
-        final String name = mCreateName.getText().toString().trim();
+        mUserName = mCreateName.getText().toString().trim();
         final String email = mCreateEmail.getText().toString().trim();
         final String pass = mCreatePassword.getText().toString().trim();
         final String passConf = mCreatePasswordConf.getText().toString().trim();
 
         boolean validEmail = checkEmail(email);
-        boolean validName = checkName(name);
+        boolean validName = checkName(mUserName);
         boolean validPassword = checkPassword(pass, passConf);
         if (!validEmail || !validName || !validPassword) return;
 
+        mProgressDialog.show();
+
         mAuth.createUserWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d("HEY", "it worked");
-                    Toast.makeText(LoginActivity.this, "New Account Created", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Sorry, New Account Failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        mProgressDialog.dismiss();
+
+                        if (task.isSuccessful()) {
+                            Log.d("HEY", "Account was created");
+                            createFirebaseUserProfile(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sorry, New Account Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 
     private boolean checkEmail(String email) {
         boolean emailOK =
@@ -151,5 +189,74 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         return true;
     }
 
+    private void createFirebaseUserProfile(final FirebaseUser user) {
 
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mUserName).build();
+        user.updateProfile(addProfileName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, user.getDisplayName());
+                        }
+                    }
+                });
+    }
+
+    private void createAuthProgressDialog() {
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("Please wait...");
+        mProgressDialog.setMessage("Registering your Account...");
+        mProgressDialog.setCancelable(false);
+    }
+
+    private void createAuthStateListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    private void timeToLogin() {
+        String email = mLoginEmail.getText().toString().trim();
+        String pass = mLoginPassword.getText().toString().trim();
+
+        boolean validEmail = checkEmail(email);
+        boolean validPassword = checkPassword(pass, pass);
+        if (!validEmail || !validPassword) return;
+
+        mLoginDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                mLoginDialog.dismiss();
+
+                Log.d(TAG, "onComplete: " + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "SigninwithEmail", task.getException());
+                    Toast.makeText(LoginActivity.this, "Sorry Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void  createLoginDialog() {
+        mLoginDialog = new ProgressDialog(this);
+        mLoginDialog.setTitle("Please wait...");
+        mLoginDialog.setMessage("Logging in...");
+        mLoginDialog.setCancelable(false);
+    }
 }
